3864c6539f4dddd36913cf22e60a4bb4d3c4c794
Sergey Kalsin <ksvk4@mail.ru>
2020-11-01 19:20:51 +0300

    Доработал

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,26 +2,24 @@ package ru.netology
 
 fun main() {
     val typeCard = "Mastercard"
-    val sumLastTransfers = 30000*100f //сумма в копейках
-    val sumTransfer = 46000*100f //сумма в копейках
+    val sumLastTransfers = 30000 * 100f //сумма в копейках
+    val sumTransfer = 46000 * 100f //сумма в копейках
 
-    print("коммиссия составила ${commission(typeCard, sumLastTransfers, sumTransfer)/100} рублей")
+    print("коммиссия составила ${commission(typeCard, sumLastTransfers, sumTransfer) / 100} рублей")
 }
 
-fun commission(typeCard:String = "VK pay", sumLastTransfers: Float = 0f, sumTransfer: Float) :Double{
-    var sumCommission: Double = 0.0
+fun commission(typeCard: String = "VK pay", sumLastTransfers: Float = 0f, sumTransfer: Float) :Double {
 
-    when(typeCard){
-        "Mastercard","Maestro" -> {
-            if (sumTransfer+sumLastTransfers > 75000*100){
-                sumCommission = (sumTransfer * (0.6/100))+20
+    when (typeCard) {
+        "Mastercard", "Maestro" -> {
+            if (sumTransfer + sumLastTransfers > 7500000) {
+                return (sumTransfer * (0.006)) + 20
             }
         }
-        "Visa","Мир" ->{
-
-            sumCommission = if (sumTransfer * (0.75/100) < 35 ) 35.0 else sumTransfer * (0.75/100)
+        "Visa", "Мир" -> {
+            return if (sumTransfer * (0.0075) < 35) 35.0 else sumTransfer * (0.0075)
         }
     }
+    return 0.0
 
-    return  sumCommission
 }
